144e660c91f89771b0a8fe1c7b90a9beda78fcaf
田島諒 <ryo_tajima@tajimaryounomac-mini.local>
2018-12-16 14:15:02 +0900

    アニメーション設定追加

--- SampleAnimation/ViewController.swift
+++ SampleAnimation/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func tapStartButton(_ sender: Any) {
+        // ビューの角を丸くする処理をアニメーションで表現する
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        
+        /* アニメーション設定 */
+        // タイミング設定
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.default)
+        // 変更前の値
+        animation.fromValue = 0
+        // 変更後の値
+        animation.toValue = 20
+        // アニメーション時間
+        animation.duration = 1
+        
+        // アニメーションをレイヤーに追加
+        targetView.layer.add(animation, forKey: "cornerRadius")
+        // アニメーション終了後の状態
+        targetView.layer.cornerRadius = 20
+    }
+    
 }
 
